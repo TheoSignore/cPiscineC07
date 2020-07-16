d08ba4f816f4e804a05402b456b383af903e00cf
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-16 11:27:45 +0200

    ex02 done

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 10:00:51 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/16 10:19:22 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/16 11:27:07 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int		ft_ultimate_range(int **range, int min, int max)
 {
 	int size;
 	int i;
-	
+
 	size = max - min;
 	if (size <= 0)
 	{
