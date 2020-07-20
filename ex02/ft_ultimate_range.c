4603ecf2744a226f08985536154a9628ed12be70
Theo SIGNORE <tsignore@e2r2p16.42.fr>
2020-07-20 21:28:44 +0200

    ex02 segfault => corrected (same error as ex01)

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,7 +6,7 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 10:00:51 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/16 11:27:07 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/20 21:27:49 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_ultimate_range(int **range, int min, int max)
 		range[0] = NULL;
 		return (0);
 	}
-	range[0] = (int *)malloc(size);
+	range[0] = (int *)malloc(sizeof(int) * size);
 	if (!range[0])
 		return (-1);
 	i = -1;
