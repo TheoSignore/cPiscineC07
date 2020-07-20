b3ff0b45d4e98bd673b25774ed2396f738a60853
Theo SIGNORE <tsignore@e2r2p16.42.fr>
2020-07-20 14:47:02 +0200

    ex03 corrected - normalized

--- ex03/ft_strjoin.c
+++ ex03/ft_strjoin.c
@@ -6,11 +6,25 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 11:57:42 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/16 17:18:14 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/20 14:46:28 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 char	*ft_strcat(char *dest, char *src)
 {
@@ -47,29 +61,25 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	int		res_size;
 	char	*res;
 
-	i = 0;
 	res_size = 0;
 	if (size == 0)
 	{
-		if(!(res = (char *)malloc(sizeof(char))))
-			return (NULL)
+		if (!(res = (char *)malloc(sizeof(char))))
+			return (NULL);
 		return (res);
 	}
-	while (i < size)
-	{
+	i = -1;
+	while (++i < size)
 		res_size += ft_strlen(strs[i]);
-		i++;
-	}
-	res_size += size * ft_strlen(sep);
-	if (!(res = (char *)malloc(res_size)))
+	res_size += (size - 1) * ft_strlen(sep);
+	if (!(res = (char *)malloc(res_size + 1)))
 		return (NULL);
+	ft_strcpy(res, strs[0]);
 	i = 0;
-	while (i < size)
+	while (++i < size)
 	{
-		if (i != 0)
-			ft_strcat(res, sep);
+		ft_strcat(res, sep);
 		ft_strcat(res, strs[i]);
-		i++;
 	}
 	return (res);
 }
